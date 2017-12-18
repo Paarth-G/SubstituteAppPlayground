6c0ca94473883834567969f87b05128b09948859
Paarth-G <paarthgarg@hotmail.com>
2017-12-18 13:55:37 -0500

    added info to subform

--- Form_Substitute.playground/Contents.swift
+++ Form_Substitute.playground/Contents.swift
@@ -1,6 +1,12 @@
+
 class SubForm {
     
-    
+    let FullName : String = "";
+    let StreetAddr : String = "";
+    let CityAddress : String = "";
+    let StateAddr : String = "";
+    let ZipCode = 00000;
+
 }
 
 class TeacherForm {
