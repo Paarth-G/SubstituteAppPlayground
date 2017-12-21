710668f5cb9b6c38380cd991e20ec848d9859e46
Paarth-G <paarthgarg@hotmail.com>
2017-12-21 16:14:20 -0500

    Added more info

--- Form_Substitute.playground/Contents.swift
+++ Form_Substitute.playground/Contents.swift
@@ -1,12 +1,25 @@
 
 class SubForm {
     
+    //Substitute's Personal Info
     let FullName : String = "";
     let StreetAddr : String = "";
     let CityAddress : String = "";
     let StateAddr : String = "";
     let ZipCode = 00000;
-
+    
+    //Class Information
+    let ClassName : String = "";
+    let TeacherName : String = ""; // "Hired By"
+    let SubjectName : String = "";
+    
+    // Info
+    let NumOfDays : Int = 0;
+    //let Days:date
+    
+    //Signature
+    let Signature : String = "";
+    //let DateSigned : date
 }
 
 class TeacherForm {
@@ -14,12 +27,6 @@ class TeacherForm {
     
 }
 
-class AdminForm {
-    
-    
-}
-
 class Manager {
-    
     
 }
